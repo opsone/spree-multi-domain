a20e68839c1c25da9b5a9795fcbc883cf730d979
Kevyn Lebouille <kevyn@opsone.net>
2015-09-01 18:10:55 +0200

    Useless files

--- app/controllers/spree/store_controller_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::StoreController.class_eval do
-
-  helper_method :current_tracker
-
-  def current_tracker
-    @current_tracker ||= Spree::Tracker.current(request.env['SERVER_NAME'])
-  end
-
-end
